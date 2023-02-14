0c90434decd231f3b1b12291b20ed8b9ab4ea0a6
Jagadeep-21 <jagasparkz3456@gmail.com>
2023-02-14 22:15:44 +0530

    commit1

---
+++ src/components/Auth/Login/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className="login">
+      <div className="loginWrapper container-md ">
+        <div className="loginLeft">
+          Image
+        </div>
+        <div className="loginRight">
+          <form className=''>
+
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
